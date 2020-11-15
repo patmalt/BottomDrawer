1c763075a6fcc277292b51fc76da436523baf605
Patrick Maltagliati <patmalt@gmail.com>
2020-11-15 13:04:55 -0600

    Create bottom drawer view.

--- BottomDrawer/ContentView.swift
+++ BottomDrawer/ContentView.swift
@@ -6,11 +6,77 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        BottomDrawer(main: Main(), detail: Detail())
+    }
+}
+
+struct BottomDrawer<Main: View, Detail: View>: View {
+    let main: () -> Main
+    let detail: () -> Detail
+    @State private var expanded: Bool = false
+
+    init(main: Main, detail: Detail) {
+        self.main = { main }
+        self.detail = { detail }
+    }
+
+    init(@ViewBuilder main: @escaping () -> Main, @ViewBuilder detail: @escaping () -> Detail) {
+        self.main = main
+        self.detail = detail
+    }
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack(alignment: .bottom) {
+                main()
+                detail()
+                    .frame(height: proxy.size.height / (expanded ? 1.1 : 4))
+                    .background(Blur().ignoresSafeArea())
+                    .onTapGesture {
+                        withAnimation { expanded.toggle() }
+                    }
+            }
+        }
+    }
+}
+
+struct Main: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.627003, longitude: -90.199402),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
+    var body: some View {
+        Map(coordinateRegion: $region).ignoresSafeArea(.all, edges: .bottom)
+    }
+}
+
+struct Blur: UIViewRepresentable { // https://medium.com/@edwurtle/blur-effect-inside-swiftui-a2e12e61e750
+    var style: UIBlurEffect.Style = .systemUltraThinMaterial
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
+
+struct Detail: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Group {
+                    Label("Hello, World", systemImage: "pencil")
+                }
+                .padding()
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
 
