b31e55685f91a1453e441aa1dc8afa5d44427ff0
Patrick Maltagliati <patmalt@gmail.com>
2020-11-15 11:44:44 -0600

    Initial Commit

---
+++ BottomDrawer/BottomDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomDrawerApp.swift
+//  BottomDrawer
+//
+//  Created by Patrick Maltagliati on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct BottomDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
